5ba62952d7e279e226f39c86166ca655ab2e4fdf
shuvo794 <shuvohossain794@gmail.com>
2023-12-18 11:41:10 +0600

    added a navber

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 
 import { Outlet } from 'react-router-dom'
 import Footer from '../pages/Share/Footer/Footer'
+import Navbar from '../pages/Share/Navbar/Navbar'
 
 function Main() {
   return (
     <div>
+      <Navbar></Navbar>
         <Outlet></Outlet>
         <Footer></Footer>
     </div>
