6040cf79c8f66b9036a4baa5b7bf9b4486d683ae
shuvo794 <shuvohossain794@gmail.com>
2023-12-18 11:12:35 +0600

    setup footer page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 
 import { Outlet } from 'react-router-dom'
+import Footer from '../pages/Share/Footer/Footer'
 
 function Main() {
   return (
     <div>
         <Outlet></Outlet>
+        <Footer></Footer>
     </div>
   )
 }
