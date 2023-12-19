830013f2ae94755a9cb4e8be444a30dc0b0d352e
shuvo794 <shuvohossain794@gmail.com>
2023-12-19 08:16:21 +0600

    added feture items

---
+++ src/pages/Home/FetureItems/FetureItems.jsx
@@ -0,0 +1,15 @@
+import SectionTittle from "../../../Components/SectionTittle/SectionTittle"
+
+
+function FetureItems() {
+  return (
+    <div>
+             <SectionTittle
+             heading={"Check it out"}
+             subHeading={"FROM OUR MENU"}
+             ></SectionTittle>
+    </div>
+  )
+}
+
+export default FetureItems
