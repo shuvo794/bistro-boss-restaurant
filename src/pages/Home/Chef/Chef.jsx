f734591c6a9e847f317976a1042ec9672d309ddb
shuvo794 <shuvohossain794@gmail.com>
2023-12-19 19:00:56 +0600

    added

--- src/pages/Home/Chef/Chef.jsx
+++ src/pages/Home/Chef/Chef.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SectionTittle from '../../../Components/SectionTittle/SectionTittle'
+import slide1 from '../../../assets/home/slide1.jpg'
 
 function Chef() {
   return (
@@ -10,6 +11,39 @@ function Chef() {
         >
 
         </SectionTittle>
+
+        <div className='grid md:grid-cols-3 gap-5"'>
+        <div className="card w-96 glass">
+  <img style={{width:'424px',height:'300px'}} src={slide1} alt="car!"/>
+  <div className="card-body ">
+    <h2 className="card-title ">Caeser Salad</h2>
+    <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+    <div className="card-actions justify-center -mb-3">
+      <button className="btn  uppercase">add to cart</button>
+    </div>
+  </div>
+</div>
+        <div className="card w-96 glass">
+  <img style={{width:'424px',height:'300px'}} src={slide1} alt="car!"/>
+  <div className="card-body ">
+    <h2 className="card-title ">Caeser Salad</h2>
+    <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+    <div className="card-actions justify-center -mb-3">
+      <button className="btn  uppercase">add to cart</button>
+    </div>
+  </div>
+</div>
+        <div className="card w-96 glass">
+  <img style={{width:'424px',height:'300px'}} src={slide1} alt="car!"/>
+  <div className="card-body ">
+    <h2 className="card-title ">Caeser Salad</h2>
+    <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+    <div className="card-actions justify-center -mb-3">
+      <button className="btn  uppercase">add to cart</button>
+    </div>
+  </div>
+</div>
+        </div>
 
     </section>
   )
