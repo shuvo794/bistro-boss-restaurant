6167e0ae84cb6647dfead4a62d295bc730f3ba45
shuvo794 <shuvohossain794@gmail.com>
2023-12-20 11:02:21 +0600

    added

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,6 +1,8 @@
 import { Helmet } from "react-helmet-async"
 import Cover from "../Share/Cover/Cover"
 import shop from '../../assets/shop/banner2.jpg'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 function Order() {
   return (
@@ -8,9 +10,18 @@ function Order() {
         <Helmet>
         <title>Bistro Boss/Our Shop</title>
     </Helmet>
-    <Cover img={shop}
-    title="OUR SHOP"
-    ></Cover>
+    <Cover img={shop}title="OUR SHOP"></Cover>
+
+    <Tabs defaultIndex={1} onSelect={(index) => console.log(index)}>
+  <TabList>
+    <Tab>Title 1</Tab>
+    <Tab>Title 2</Tab>
+  </TabList>
+  <TabPanel></TabPanel>
+  <TabPanel></TabPanel>
+</Tabs>
+
+
     </div>
   )
 }
