1160d548fd163fc654e9f56054f2140cd7cedfaa
shuvo794 <shuvohossain794@gmail.com>
2023-12-20 13:09:03 +0600

    added

--- src/pages/Home/FetureItems/FetureItems.jsx
+++ src/pages/Home/FetureItems/FetureItems.jsx
@@ -20,7 +20,8 @@ function FetureItems() {
                     <p>Dec 20, 2023</p>
                     <p>WHERE CAN I GET SOME?</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
-                    <button className="btn btn-outline border-0 border-h border-b-4 mt-4">Read More</button>
+                 
+                    <button className="btn btn-outline border-0 border-h border-b-4 mt-4 ">Read More</button>
                 </div>
              </div>
         </div>
