1ce8bc5021b1cc2e06ed23b5e3e8e551d44ef826
shuvo794 <shuvohossain794@gmail.com>
2023-12-22 08:48:50 +0600

    added

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -8,11 +8,11 @@ function FoodCard({item}) {
         <div className="card w-96 bg-base-100 shadow-xl">
   <figure><img src={image} alt="Shoes" /></figure>
   <p className='absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white'>${price}</p>
-  <div className="card-body">
+  <div className="card-body flex flex-col items-center">
     <h2 className="card-title">{name}</h2>
     <p>{recipe}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Add To Cart</button>
+      <button className="btn btn-outline border-0 border-h bg-slate-100 border-orange-400 border-b-4 mt-4">Add To Cart</button>
     </div>
   </div>
 </div>
--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
+// import React from 'react'
 import FoodCard from '../../../Components/FoodCard/FoodCard'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 function OrderTab({items}) {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  };
   return (
-    <div className="grid md:grid-cols-3 gap-10">
-    {
+    
+      
+       
+       <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+       <SwiperSlide>
+       <div className="grid md:grid-cols-3 gap-10">
+       {
       items.map(item=><FoodCard
       key={item._id}
       item={item}
       ></FoodCard>)
     }
-    </div>
+       </div>
+        </SwiperSlide>
+        </Swiper>
+       
+       
+     
+   
+    
   )
 }
 
