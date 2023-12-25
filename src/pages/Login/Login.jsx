c9a8a0af0dea77b95c1c3fc81d234950a78669d1
shuvo794 <shuvohossain794@gmail.com>
2023-12-25 13:33:01 +0600

    login page setup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,14 @@
 
 
 function Login() {
+    
+    const hundelLogIn = event =>{
+          event.preventDefault();
+          const form = event.target;
+          const email = form.email.value;
+          const password=form.password.value;
+          console.log(email,password);
+    }
   return (
     <div className="hero min-h-screen bg-base-200">
     <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,24 +17,25 @@ function Login() {
         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       </div>
       <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        <form className="card-body">
+        <form onSubmit={hundelLogIn} className="card-body">
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text" >Email</span>
             </label>
-            <input type="email" placeholder="email" className="input input-bordered" required />
+            <input type="email" placeholder="email" name="email" className="input input-bordered" required />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input type="password" placeholder="password" className="input input-bordered" required />
+            <input type="password" placeholder="password" name="password" className="input input-bordered" required />
             <label className="label">
               <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
             </label>
           </div>
           <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
+            
+            <input className="btn btn-primary" type="submit" value="Submit" />
           </div>
         </form>
       </div>
