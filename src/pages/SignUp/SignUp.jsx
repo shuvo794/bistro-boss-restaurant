34ce75686a8172e6bf442f521b7146a9d118fb61
shuvo794 <shuvohossain794@gmail.com>
2023-12-27 19:04:52 +0600

    added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -39,7 +39,7 @@ function SignUp() {
           <input type="email" placeholder="email" name="email" {...register("email",{ required: true })} className="input input-bordered"  />
           {errors.email && <span className="text-red-600">Email field is required</span>}
         </div>
-        <div className="form-control">
+        {/* <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
@@ -48,7 +48,26 @@ function SignUp() {
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
-        </div>
+        </div> */}
+
+<div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password"  {...register("password", {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                })} placeholder="password" className="input input-bordered" />
+                                {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary">Sign Up</button>
         </div>
