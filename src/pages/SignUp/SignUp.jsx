bf434eaf91ae19ac0e9f37269163b4b73b083d47
shuvo794 <shuvohossain794@gmail.com>
2023-12-27 11:05:09 +0600

    created a sigin page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -32,7 +32,7 @@ function SignUp() {
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary">Sign Up</button>
         </div>
       </form>
     </div>
