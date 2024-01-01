96fc5c93bec74afee9feb8a23beb96e6fa429249
shuvo794 <shuvohossain794@gmail.com>
2024-01-01 18:57:06 +0600

    send

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -17,7 +17,7 @@ function FoodCard({item}) {
     <h2 className="card-title">{name}</h2>
     <p>{recipe}</p>
     <div className="card-actions justify-end">
-      <button onClick={()=>handelAddTocurt()} className="btn btn-outline border-0 border-h bg-slate-100 border-orange-400 border-b-4 mt-4">Add To Cart</button>
+      <button onClick={()=>handelAddTocurt(item)} className="btn btn-outline border-0 border-h bg-slate-100 border-orange-400 border-b-4 mt-4">Add To Cart</button>
     </div>
   </div>
 </div>
