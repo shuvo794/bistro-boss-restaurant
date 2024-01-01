ca2bdb6ba5394ef1746413f5a6c3cc7572580bbc
shuvo794 <shuvohossain794@gmail.com>
2024-01-01 18:56:19 +0600

    send

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 function FoodCard({item}) {
 
   const { name, image, recipe, price } = item;
+
+  const handelAddTocurt = item => {
+    
+  }
   
   return (
     <div>
@@ -13,7 +17,7 @@ function FoodCard({item}) {
     <h2 className="card-title">{name}</h2>
     <p>{recipe}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-outline border-0 border-h bg-slate-100 border-orange-400 border-b-4 mt-4">Add To Cart</button>
+      <button onClick={()=>handelAddTocurt()} className="btn btn-outline border-0 border-h bg-slate-100 border-orange-400 border-b-4 mt-4">Add To Cart</button>
     </div>
   </div>
 </div>
