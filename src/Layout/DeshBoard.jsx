eef2a032709f8dd9e3d2e4169744b9930b0b68bd
shuvo794 <shuvohossain794@gmail.com>
2024-01-02 21:46:22 +0600

    my cart section work

--- src/Layout/DeshBoard.jsx
+++ src/Layout/DeshBoard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 
 function DeshBoard() {
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col items-center justify-center">
-        
+        <Outlet></Outlet>
         <label
           htmlFor="my-drawer-2"
           className="btn btn-primary drawer-button lg:hidden"
