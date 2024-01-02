fb6d5aaf52b3284b5f59630e52c5d625eecd102c
shuvo794 <shuvohossain794@gmail.com>
2024-01-02 11:16:08 +0600

    use carts page

--- src/pages/Hooks/usecarts.jsx
+++ src/pages/Hooks/usecarts.jsx
@@ -1,3 +1,9 @@
-const usecarts = email => {
-    
-}
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+
+const usecarts = () => {
+  const { user } = useContext(AuthContext);
+};
+
+export default usecarts;
