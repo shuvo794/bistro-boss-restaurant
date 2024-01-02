d02cb6a1b52aaa6b6156118c732ec0b34ae21858
shuvo794 <shuvohossain794@gmail.com>
2024-01-02 11:32:54 +0600

    use carts page

--- src/pages/Hooks/usecarts.jsx
+++ src/pages/Hooks/usecarts.jsx
@@ -3,15 +3,17 @@ import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 
 const usecarts = () => {
-    const { user } = useContext(AuthContext);
-     const { isLoading, data:cart=[]} = useQuery({
-       queryKey: ["cart",user.email],
-         queryFn: async () => {
-             const response = await fetch(`http://localhost:5000/carts=${user.email}`);
-             return response.json();
-       },
-     });
-    return[cart,isLoading]
+  const { user } = useContext(AuthContext);
+  const { isLoading, data: cart = [] } = useQuery({
+    queryKey: ["cart", user.email],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:5000/carts=${user?.email}`
+      );
+      return response.json();
+    },
+  });
+  return [cart, isLoading];
 };
 
 export default usecarts;
