09e89ac355b28d8d548cd47b06c0b11d909a44fa
shuvo794 <shuvohossain794@gmail.com>
2024-01-02 21:44:27 +0600

    send fromating set up route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,4 @@
-import {
-    createBrowserRouter,
-    
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
@@ -13,45 +10,49 @@ import Secret from "../pages/Share/Secret/Secret";
 import DeshBoard from "../Layout/DeshBoard";
 import MyCart from "../pages/DeshBoard/MyCart/MyCart";
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children:[
-        {
-            path:'/',
-            element:<Home></Home>
-        },
-        {
-          path:'menu',
-          element:<Menu></Menu>
-        },
-        {
-          path:'order/:category',
-          element:<Order></Order>
-        },
-        {
-          path:'login',
-          element:<Login></Login>
-        },
-        {
-         path:'signup',
-         element:<SignUp></SignUp>
-        },
-        {
-          path: 'secret',
-          element:<PrivateRoute><Secret></Secret></PrivateRoute>
-        }
-      ]
-    },
-    {
-      path: "deshboard",
-      element: <DeshBoard></DeshBoard>,
-      children: [
-        {
-          path: 'mycart',
-          element:<MyCart></MyCart>
-       }
-      ]
-    }
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "menu",
+        element: <Menu></Menu>,
+      },
+      {
+        path: "order/:category",
+        element: <Order></Order>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: "secret",
+        element: (
+          <PrivateRoute>
+            <Secret></Secret>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "deshboard",
+    element: <DeshBoard></DeshBoard>,
+    children: [
+      {
+        path: "mycart",
+        element: <MyCart></MyCart>,
+      },
+    ],
+  },
+]);
