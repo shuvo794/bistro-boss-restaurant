16324985356ed0cf99e0a32b9f368ecd1ca0a924
shuvo794 <shuvohossain794@gmail.com>
2024-01-02 11:09:31 +0600

    setup transatck quary

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import {
-  
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import { router } from './Routes/Routes';
-import { HelmetProvider } from 'react-helmet-async';
-import AuthProvider from './Provider/AuthProvider';
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { router } from "./Routes/Routes";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Provider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-    <HelmetProvider>
-   <div className='max-w-screen-xl mx-auto'>
-   <RouterProvider router={router} />
-   </div>
-   </HelmetProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <div className="max-w-screen-xl mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
+      </HelmetProvider>
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/Hooks/usecarts.jsx
@@ -0,0 +1,3 @@
+const usecarts = email => {
+    
+}
