8390c6b6d1a345c02c70928e0ff2d1841cfb91d8
shuvo794 <shuvohossain794@gmail.com>
2024-01-03 13:06:26 +0600

    work my cart section

--- src/pages/DeshBoard/MyCart/MyCart.jsx
+++ src/pages/DeshBoard/MyCart/MyCart.jsx
@@ -41,29 +41,23 @@ function MyCart() {
                   {index+1}
                 </td>
                 <td>
-                  <div className="flex items-center gap-3">
+                 
                     <div className="avatar">
                       <div className="mask mask-squircle w-12 h-12">
                         <img
-                          src="/tailwind-css-component-profile-2@56w.png"
+                          src={row.image}
                           alt="Avatar Tailwind CSS Component"
                         />
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
-                  </div>
+                    
+                 
                 </td>
                 <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    Desktop Support Technician
-                  </span>
+                  {row.name}
+                  
                 </td>
-                <td>Purple</td>
+                <td>$ {row.price}</td>
                 <td>
                   <button className="btn btn-ghost btn-xs">details</button>
                 </td>
