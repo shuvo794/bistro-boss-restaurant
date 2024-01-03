5115249a1190d979eccfd400d50a72c7773cdd12
shuvo794 <shuvohossain794@gmail.com>
2024-01-03 12:17:11 +0600

    work my cart section

--- src/pages/DeshBoard/MyCart/MyCart.jsx
+++ src/pages/DeshBoard/MyCart/MyCart.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async';
+import usecarts from '../../Hooks/usecarts';
+import SectionTittle from '../../../Components/SectionTittle/SectionTittle';
 
 function MyCart() {
+  const [cart] = usecarts()
+  const total =cart.reduce((sum,item)=>item.price+sum,0)
   return (
-    <div>
+    <section>
       <Helmet>
         <title>Bistro Boss/myCart</title>
       </Helmet>
-      <h2>My cart</h2>
-    </div>
+
+      {/* <SectionTittle
+        subHeading={"My Cart"}
+        heading={"WANNA ADD MORE?"}
+      ></SectionTittle> */}
+      <h2 className='text-3xl'>Total Orders: {cart.length}</h2>
+      <h2 className='text-3xl'>Total Price: ${total}</h2>
+    </section>
   );
 }
 
