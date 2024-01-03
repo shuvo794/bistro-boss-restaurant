ea489b3391bd56f5bf98205e57c19f36361a31e6
shuvo794 <shuvohossain794@gmail.com>
2024-01-03 12:42:10 +0600

    work my cart section

--- src/pages/DeshBoard/MyCart/MyCart.jsx
+++ src/pages/DeshBoard/MyCart/MyCart.jsx
@@ -181,15 +181,7 @@ function MyCart() {
             </tr>
           </tbody>
           {/* foot */}
-          <tfoot>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
-            </tr>
-          </tfoot>
+          
         </table>
       </div>
     </div>
