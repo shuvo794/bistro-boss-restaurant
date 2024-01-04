2644b5ba30a80b3f2c79c0d400c434b97102f068
shuvo794 <shuvohossain794@gmail.com>
2024-01-04 13:59:45 +0600

    work a total user tabil

--- src/pages/DeshBoard/AllUsers/AllUsers.jsx
+++ src/pages/DeshBoard/AllUsers/AllUsers.jsx
@@ -14,7 +14,30 @@ function AllUsers() {
     
   return (
     <div>
-      <h2>All User : {users.length}</h2>
+      <h2 className="text-3xl font-semibold	">All User : {users.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead className="bg-[#D1A054] text-white">
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>EMAIL</th>
+              <th>ROLE</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
