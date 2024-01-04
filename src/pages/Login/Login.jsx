3c5aede9f8f6bf2d1aa250cd04a35dc97e83ad65
shuvo794 <shuvohossain794@gmail.com>
2024-01-04 09:56:05 +0600

    create btn & devider added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -9,6 +9,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import Swal from 'sweetalert2'
 import authincation1 from '../../assets/others/authentication2.png';
+import SocalSite from "../Share/SocalSite/SocalSite";
 function Login() {
   
   const [disable, setDisable] = useState(true);
@@ -62,75 +63,72 @@ function Login() {
     <>
       <Helmet>
         <title>Bistro Boss/Login</title>
-        
       </Helmet>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="card  md:w-1/2  max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={hundelLogIn} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                name="email"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                name="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <LoadCanvasTemplate />
-              </label>
-              <input
-              onBlur={hendelCaptca}
-                type="text"
-                placeholder="type text avobe"
-                name="Captca"
-                className="input input-bordered"
-                required
-              />
-              
-            </div>
-            <div className="form-control mt-6">
-              <input
-                disabled={disable}
-                className="btn btn-primary"
-                type="submit"
-                value="LOGIN"
-              />
-            </div>
-            <p className="text-red-700">
-              New here ? <Link to="/signup">Please Create Acount</Link>{" "}
-            </p>
-          </form>
+            <form onSubmit={hundelLogIn} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <LoadCanvasTemplate />
+                </label>
+                <input
+                  onBlur={hendelCaptca}
+                  type="text"
+                  placeholder="type text avobe"
+                  name="Captca"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  disabled={disable}
+                  className="btn btn-primary"
+                  type="submit"
+                  value="LOGIN"
+                />
+              </div>
+              <p className="text-red-700">
+                New here ? <Link to="/signup">Please Create Acount</Link>{" "}
+              </p>
+            </form>
+            <SocalSite></SocalSite>
+          </div>
+          <div className="md:w-1/2  p-15 ">
+            <img src={authincation1} alt="" />
+          </div>
         </div>
-        <div className="md:w-1/2  p-15 ">
-         <img src={authincation1} alt="" />
-        </div>
-        
       </div>
-    </div>
-      
     </>
   );
 }
