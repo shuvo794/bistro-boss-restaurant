63759b22273b3437470482a18961461854915ce0
shuvo794 <shuvohossain794@gmail.com>
2024-01-04 14:20:33 +0600

    send

--- src/pages/DeshBoard/AllUsers/AllUsers.jsx
+++ src/pages/DeshBoard/AllUsers/AllUsers.jsx
@@ -1,6 +1,7 @@
 
 
 import { useQuery } from "@tanstack/react-query";
+import { FaTrashAlt } from "react-icons/fa";
 
 
 function AllUsers() {
@@ -10,13 +11,19 @@ function AllUsers() {
          const response = await fetch("http://localhost:5000/users");
          return response.json();
        },
-     });
+    });
+  
+  const handelClick = user => {
+    
+  }
     
   return (
-    <div>
-      <h2 className="text-3xl font-semibold	">All User : {users.length}</h2>
-      <div className="overflow-x-auto">
-        <table className="table table-zebra">
+    <div className="w-full">
+      <h2 className="text-3xl font-semibold	my-4 p-4">
+        All User : {users.length}
+      </h2>
+      <div className="overflow-x-auto p-4">
+        <table className="table table-zebra ">
           {/* head */}
           <thead className="bg-[#D1A054] text-white">
             <tr>
@@ -28,13 +35,22 @@ function AllUsers() {
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>Blue</td>
+                <td>
+                  <button
+                    onClick={() => handelClick(user)}
+                    className="btn btn-ghost bg-red-600 text-white "
+                  >
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
