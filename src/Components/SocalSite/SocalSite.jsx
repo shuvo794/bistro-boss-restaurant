c6f57dccffda050a07a5b1881115e749de24e7c4
shuvo794 <shuvohossain794@gmail.com>
2024-01-04 10:38:38 +0600

    send

--- src/Components/SocalSite/SocalSite.jsx
+++ src/Components/SocalSite/SocalSite.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { FaGoogle } from "react-icons/fa";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 function SocalSite() {
+  const { googleSignInUser } = useContext(AuthContext);
   return (
     <div className="w-full my-4 text-center">
       <div className="divider"></div>
