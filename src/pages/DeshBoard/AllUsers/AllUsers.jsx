e90813dc3c116fa2924cb8e7ebecf2a099805a47
shuvo794 <shuvohossain794@gmail.com>
2024-01-04 12:54:08 +0600

    solve error

--- src/pages/DeshBoard/AllUsers/AllUsers.jsx
+++ src/pages/DeshBoard/AllUsers/AllUsers.jsx
@@ -1,9 +1,22 @@
 
 
+import { useQuery } from "@tanstack/react-query";
+
+
 function AllUsers() {
+    const { refetch, data: users = [] } = useQuery({
+       queryKey: ["users"],
+       queryFn: async () => {
+         const response = await fetch("http://localhost:5000/users");
+         return response.json();
+       },
+     });
+    
   return (
-    <div>AllUsers</div>
-  )
+    <div>
+      <h2>All User : {users.length}</h2>
+    </div>
+  );
 }
 
 export default AllUsers
