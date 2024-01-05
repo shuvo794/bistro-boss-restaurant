38d43b2a638814795d63c3792c0463776eb120fb
shuvo794 <shuvohossain794@gmail.com>
2024-01-05 10:50:37 +0600

    send

--- src/pages/Hooks/usecarts.jsx
+++ src/pages/Hooks/usecarts.jsx
@@ -1,12 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
 
 const usecarts = () => {
   const { user } = useContext(AuthContext);
   const token = localStorage.getItem('access-token')
+  const axios = useAxiosSecure();
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
+    queryFn: async () => {
+  //     const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`,
+  //       {
+  //         headers: {
+  //         authorization:`barrer ${token}`
+  //       }
+  //     }
+  //     );
+  //     return response.json();
+  //   },
+  // });
     queryFn: async () => {
       const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`,
         {
