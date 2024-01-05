3d272249cdc4f6e5f1ea410886ca9058d17466fb
shuvo794 <shuvohossain794@gmail.com>
2024-01-05 09:30:22 +0600

    send

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -11,34 +11,27 @@ import img6 from '../../../assets/banner/06.png'
 
 function Banner() {
   return (
-    <Carousel infiniteLoop autoPlay>
-                <div>
-                    <img src={img1} />
-                   
-                </div>
-                <div>
-                    <img src={img2} />
-                   
-                </div>
-                <div>
-                    <img src={img3} />
-                   
-                </div>
-                <div>
-                    <img src={img4} />
-                   
-                </div>
-                <div>
-                    <img src={img5} />
-                   
-                </div>
-                <div>
-                    <img src={img6} />
-                   
-                </div>
-                
-            </Carousel>
-  )
+    <Carousel showArrows={false} infiniteLoop autoPlay>
+      <div>
+        <img src={img1} />
+      </div>
+      <div>
+        <img src={img2} />
+      </div>
+      <div>
+        <img src={img3} />
+      </div>
+      <div>
+        <img src={img4} />
+      </div>
+      <div>
+        <img src={img5} />
+      </div>
+      <div>
+        <img src={img6} />
+      </div>
+    </Carousel>
+  );
 }
 
 export default Banner
--- src/pages/Home/Catagory/Catagory.jsx
+++ src/pages/Home/Catagory/Catagory.jsx
@@ -1,9 +1,10 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Pagination } from "swiper/modules";
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+
 
 import catagory1 from '../../../assets/home/slide1.jpg'
 import catagory2 from '../../../assets/home/slide2.jpg'
@@ -16,52 +17,57 @@ import SectionTittle from '../../../Components/SectionTittle/SectionTittle';
 
 function Catagory() {
   return (
-    <section className='mb-24'>
-      
-    <SectionTittle
-    subHeading={"From 11:00am to 10:00pm"}
-    heading={"ORDER ONLINE"}
-    >
-      </SectionTittle>
-     
+    <section className="mb-24">
+      <SectionTittle
+        subHeading={"From 11:00am to 10:00pm"}
+        heading={"ORDER ONLINE"}
+      ></SectionTittle>
+
       <Swiper
-       
-    slidesPerView={4}
-    spaceBetween={30}
-    centeredSlides={true}
-    pagination={{
-      clickable: true,
-      
-    }}
-    modules={[Pagination]}
-        className="mySwiper"
         
-  >
-    <SwiperSlide>
-    <img src={catagory2} alt="" />
-    <h2 className='text-4xl uppercase text-center -mt-14 text-white'>Pizza</h2>
-    </SwiperSlide>
-    <SwiperSlide>
-        <img src={catagory1} alt="" />
-        <h2 className='text-4xl uppercase text-center -mt-14 text-white'>Salad</h2>
-    </SwiperSlide>
-    
-    <SwiperSlide>
-    <img src={catagory3} alt="" />
-    <h2 className='text-4xl uppercase text-center -mt-14 text-white'>Soups</h2>
-    </SwiperSlide>
-    <SwiperSlide>
-    <img src={catagory4} alt="" />
-    <h2 className='text-4xl uppercase text-center -mt-14 text-white'>Cake</h2>
-    </SwiperSlide>
-    <SwiperSlide>
-    <img src={catagory5} alt="" />
-    <h2 className='text-4xl uppercase text-center -mt-14 text-white'>Salad</h2>
-    </SwiperSlide> 
-    
-  </Swiper>
-   </section>
-  )
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={false}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={catagory2} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-14 text-white">
+            Pizza
+          </h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={catagory1} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-14 text-white">
+            Salad
+          </h2>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src={catagory3} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-14 text-white">
+            Soups
+          </h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={catagory4} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-14 text-white">
+            Cake
+          </h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={catagory5} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-14 text-white">
+            Salad
+          </h2>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
 }
 
 export default Catagory
