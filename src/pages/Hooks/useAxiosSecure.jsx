f3c65004d2ef54c709457253b9e6c87b3826c674
shuvo794 <shuvohossain794@gmail.com>
2024-01-06 21:39:55 +0600

    send code

--- src/pages/Hooks/useAxiosSecure.jsx
+++ src/pages/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
-// import useAuth from "./useAuth";
+
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
