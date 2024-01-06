7709841999477079b09f2c1e6f27742e2cc5d69e
shuvo794 <shuvohossain794@gmail.com>
2024-01-06 21:41:52 +0600

    send code

--- src/pages/Hooks/useMenu.jsx
+++ src/pages/Hooks/useMenu.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 
-const useMenu=()=>{
-    const [menu,setMenu]=useState([]);
-    const [loading,SetLoading]=useState(true)
+const useMenu = () => {
+  const [menu, setMenu] = useState([]);
+  const [loading, SetLoading] = useState(true);
 
-    useEffect(()=>{
-        fetch('http://localhost:5000/menu')
-        .then(res=>res.json())
-        .then(data=>{
-            setMenu(data)
-            SetLoading(false)
-        })
-    },[])
+  useEffect(() => {
+    fetch("http://localhost:5000/menu")
+      .then((res) => res.json())
+      .then((data) => {
+        setMenu(data);
+        SetLoading(false);
+      });
+  }, []);
 
-    return [menu,loading]
-}
+  return [menu, loading];
+};
 
 export default useMenu;
