3c0cff8664a75f6378d0e55fba8175bbdf95c3ea
shuvo794 <shuvohossain794@gmail.com>
2024-01-07 21:37:59 +0600

    code formate

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,13 +1,11 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import img1 from '../../../assets/banner/01.jpg'
-import img2 from '../../../assets/banner/02.jpg'
-import img3 from '../../../assets/banner/03.png'
-import img4 from '../../../assets/banner/04.jpg'
-import img5 from '../../../assets/banner/05.png'
-import img6 from '../../../assets/banner/06.png'
-
-
+import { Carousel } from "react-responsive-carousel";
+import img1 from "../../../assets/banner/01.jpg";
+import img2 from "../../../assets/banner/02.jpg";
+import img3 from "../../../assets/banner/03.png";
+import img4 from "../../../assets/banner/04.jpg";
+import img5 from "../../../assets/banner/05.png";
+import img6 from "../../../assets/banner/06.png";
 
 function Banner() {
   return (
@@ -34,4 +32,4 @@ function Banner() {
   );
 }
 
-export default Banner
+export default Banner;
