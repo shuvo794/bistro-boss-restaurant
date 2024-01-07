d55e453cd011816b1b0b06ca6456e8f9c843a187
shuvo794 <shuvohossain794@gmail.com>
2024-01-07 21:34:20 +0600

    send code

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,32 +1,30 @@
-import { Helmet } from "react-helmet-async"
-import Banner from "../Banner/Banner"
-import BistroSection from "../BistroSection/BistroSection"
-import CallUs from "../CallUs/CallUs"
-import Catagory from "../Catagory/Catagory"
-import Chef from "../Chef/Chef"
-import FetureItems from "../FetureItems/FetureItems"
-import PopulerMenu from "../PopulerMenu/PopulerMenu"
-import Reviwe from "../Reviwe/Reviwe"
-
+import { Helmet } from "react-helmet-async";
+import Banner from "../Banner/Banner";
+import BistroSection from "../BistroSection/BistroSection";
+import CallUs from "../CallUs/CallUs";
+import Catagory from "../Catagory/Catagory";
+import Chef from "../Chef/Chef";
+import FetureItems from "../FetureItems/FetureItems";
+import PopulerMenu from "../PopulerMenu/PopulerMenu";
+import Reviwe from "../Reviwe/Reviwe";
 
 function Home() {
   return (
     <div>
-        <Helmet>
+      <Helmet>
         <title>Bistro Boss/Home</title>
-        
       </Helmet>
-      
+
       <Banner></Banner>
       <Catagory></Catagory>
-      <BistroSection></BistroSection> 
+      <BistroSection></BistroSection>
       <PopulerMenu></PopulerMenu>
       <CallUs></CallUs>
       <Chef></Chef>
       <FetureItems></FetureItems>
       <Reviwe></Reviwe>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
