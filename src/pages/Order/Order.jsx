8bfc5f926e73d1723add4cfa408b25ab813aa668
shuvo794 <shuvohossain794@gmail.com>
2024-01-07 21:26:22 +0600

    send code

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,8 +1,8 @@
-import { Helmet } from "react-helmet-async"
-import Cover from "../Share/Cover/Cover"
-import shop from '../../assets/shop/banner2.jpg'
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { Helmet } from "react-helmet-async";
+import Cover from "../Share/Cover/Cover";
+import shop from "../../assets/shop/banner2.jpg";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import { useState } from "react";
 import useMenu from "../Hooks/useMenu";
 
@@ -10,53 +10,51 @@ import OrderTab from "./OrderTab/OrderTab";
 import { useParams } from "react-router-dom";
 
 function Order() {
-  const categories =["salad","pizza","soup","dessert","drinks"]
-  const {category}=useParams();
-  const initialIndex=categories.indexOf(category)
-    const [tabIndex,setTabIndex]=useState(initialIndex);
-   
-    console.log(category);
-    const [menu]=useMenu();
-    const dessert =menu.filter(item=>item.category === 'dessert');
-    const pizza =menu.filter(item=>item.category === 'pizza');
-    const salad =menu.filter(item=>item.category === 'salad');
-    const soup =menu.filter(item=>item.category === 'soup');
-    const drinks=menu.filter(item=>item.category === 'drinks');
+  const categories = ["salad", "pizza", "soup", "dessert", "drinks"];
+  const { category } = useParams();
+  const initialIndex = categories.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialIndex);
+
+  console.log(category);
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const drinks = menu.filter((item) => item.category === "drinks");
   return (
     <div>
-        <Helmet>
+      <Helmet>
         <title>Bistro Boss/Our Shop</title>
-    </Helmet>
-    <Cover img={shop}title="OUR SHOP"></Cover>
+      </Helmet>
+      <Cover img={shop} title="OUR SHOP"></Cover>
 
-    <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-  <TabList>
-    <Tab>Salad</Tab>
-    <Tab>Pizza</Tab>
-    <Tab>Soups</Tab>
-    <Tab>Desserts</Tab>
-    <Tab>Drinks</Tab>
-  </TabList>
-  <TabPanel>
-   <OrderTab items={salad}></OrderTab>
-  </TabPanel>
-  <TabPanel>
-   <OrderTab items={pizza}></OrderTab>
-  </TabPanel>
-  <TabPanel>
-   <OrderTab items={soup}></OrderTab>
-  </TabPanel>
-  <TabPanel>
-   <OrderTab items={dessert}></OrderTab>
-  </TabPanel>
-  <TabPanel>
-   <OrderTab items={drinks}></OrderTab>
-  </TabPanel>
-</Tabs>
-
-
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soups</Tab>
+          <Tab>Desserts</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel>
+          <OrderTab items={salad}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={pizza}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={soup}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={dessert}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={drinks}></OrderTab>
+        </TabPanel>
+      </Tabs>
     </div>
-  )
+  );
 }
 
-export default Order
+export default Order;
