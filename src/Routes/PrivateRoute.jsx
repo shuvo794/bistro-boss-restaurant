2e74a991d68cbcfdf0eb6344e8e9c7dc71830b41
shuvo794 <shuvohossain794@gmail.com>
2024-01-09 12:14:25 +0600

    solve code

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,9 +1,9 @@
-import { useContext } from "react";
-import { AuthContext } from "../Provider/AuthProvider";
+
 import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../pages/Hooks/useAuth";
 
 function PrivateRoute({ children }) {
-  const { user, loading } = useContext(AuthContext);
+  const { user, loading } = useAuth;
   const location = useLocation();
 
   if (loading) {
