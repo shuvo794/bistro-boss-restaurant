07ff655be5dd76b0b349a9d45a922ad6e12be783
shuvo794 <shuvohossain794@gmail.com>
2024-01-09 10:49:38 +0600

    send

--- src/pages/Hooks/usecarts.jsx
+++ src/pages/Hooks/usecarts.jsx
@@ -1,24 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 
 import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
 
 const usecarts = () => {
   const { user } = useAuth();
 
-  const token = localStorage.getItem("access-token");
-  // const [axiosSecure] = useAxiosSecure();
+  // const token = localStorage.getItem("access-token");
+  const [axiosSecure] = useAxiosSecure();
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
-      const response = await fetch(
-        `http://localhost:5000/carts?email=${user?.email}`,
-        {
-          headers: {
-            authorization: `barrer ${token}`,
-          },
-        }
+      const response = await axiosSecure.get(
+        `/carts?email=${user?.email}`
       );
-      return response.json();
+      return response.data;
     },
 
 // Todo Axcios note use now 
