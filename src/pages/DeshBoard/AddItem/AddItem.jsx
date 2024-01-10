68a66b61bea3c6aa2c7bc5b46046d67addb24b7a
shuvo794 <shuvohossain794@gmail.com>
2024-01-10 08:07:06 +0600

    send

---
+++ src/pages/DeshBoard/AddItem/AddItem.jsx
@@ -0,0 +1,15 @@
+import SectionTittle from "../../../Components/SectionTittle/SectionTittle";
+
+
+function AddItem() {
+  return (
+    <div>
+      <SectionTittle
+        subHeading={"Should Try"}
+        heading={"RECOMMENDS"}
+      ></SectionTittle>
+    </div>
+  );
+}
+
+export default AddItem
