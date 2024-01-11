c15b66180130ca1a950a72e6d46310996ee5d119
shuvo794 <shuvohossain794@gmail.com>
2024-01-11 22:06:23 +0600

    send

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -8,7 +8,7 @@ function PrivateRoute({ children }) {
   if (loading) {
     return <progress className="progress w-56"></progress>;
   }
-  if (user && user.uid) {
+  if (user) {
     return children;
   }
   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
