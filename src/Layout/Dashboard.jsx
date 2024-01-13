6856b91864c904a53a550069caab021654a2fec7
shuvo794 <shuvohossain794@gmail.com>
2024-01-13 08:54:22 +0600

    edit desgin

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -11,91 +11,104 @@ const Dashboard = () => {
     const [isAdmin] = useAdmin();
 
     return (
-        <div className="flex">
-            {/* dashboard side bar */}
-            <div className="w-64 min-h-screen bg-red-500">
-                <ul className="menu p-4">
-                    {
-                        isAdmin ? <>
-                            <li>
-                                <NavLink to="/dashboard/adminHome">
-                                    <FaHome></FaHome>
-                                    Admin Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/addItems">
-                                    <FaUtensils></FaUtensils>
-                                    Add Items</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/manageItems">
-                                    <FaList></FaList>
-                                    Manage Items</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/bookings">
-                                    <FaBook></FaBook>
-                                    Manage Bookings</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/users">
-                                    <FaUsers></FaUsers>
-                                    All Users</NavLink>
-                            </li>
-                        </>
-                            :
-                            <>
-                                <li>
-                                    <NavLink to="/dashboard/userHome">
-                                        <FaHome></FaHome>
-                                        User Home</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/reservation">
-                                        <FaCalendar></FaCalendar>
-                                        Reservation</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/cart">
-                                        <FaShoppingCart></FaShoppingCart>
-                                        My Cart ({cart.length})</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/review">
-                                        <FaAd></FaAd>
-                                        Add a Review</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/bookings">
-                                        <FaList></FaList>
-                                        My Bookings</NavLink>
-                                </li>
-                            </>
-                    }
-                    {/* shared nav links */}
-                    <div className="divider"></div>
-                    <li>
-                        <NavLink to="/">
-                            <FaHome></FaHome>
-                            Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/order/salad">
-                            <FaSearch></FaSearch>
-                            Menu</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/order/contact">
-                            <FaEnvelope></FaEnvelope>
-                            Contact</NavLink>
-                    </li>
-                </ul>
-            </div>
-            {/* dashboard content */}
-            <div className="flex-1 p-8">
-                <Outlet></Outlet>
-            </div>
+      <div className="flex">
+        {/* dashboard side bar */}
+        <div className="w-64 min-h-screen bg-[#c23616] text-white">
+          <ul className="menu p-4">
+            {isAdmin ? (
+              <>
+                <li>
+                  <NavLink to="/dashboard/adminHome">
+                    <FaHome></FaHome>
+                    Admin Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/addItems">
+                    <FaUtensils></FaUtensils>
+                    Add Items
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/manageItems">
+                    <FaList></FaList>
+                    Manage Items
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/bookings">
+                    <FaBook></FaBook>
+                    Manage Bookings
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/users">
+                    <FaUsers></FaUsers>
+                    All Users
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <NavLink to="/dashboard/userHome">
+                    <FaHome></FaHome>
+                    User Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/reservation">
+                    <FaCalendar></FaCalendar>
+                    Reservation
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/cart">
+                    <FaShoppingCart></FaShoppingCart>
+                    My Cart ({cart.length})
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/review">
+                    <FaAd></FaAd>
+                    Add a Review
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/dashboard/bookings">
+                    <FaList></FaList>
+                    My Bookings
+                  </NavLink>
+                </li>
+              </>
+            )}
+            {/* shared nav links */}
+            <div className="divider"></div>
+            <li>
+              <NavLink to="/">
+                <FaHome></FaHome>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/order/salad">
+                <FaSearch></FaSearch>
+                Menu
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/order/contact">
+                <FaEnvelope></FaEnvelope>
+                Contact
+              </NavLink>
+            </li>
+          </ul>
         </div>
+        {/* dashboard content */}
+        <div className="flex-1 p-8">
+          <Outlet></Outlet>
+        </div>
+      </div>
     );
 };
 
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -10,26 +10,26 @@ import img6 from '../../../assets/home/06.png';
 
 const Banner = () => {
     return (
-        <Carousel>
-            <div>
-                <img src={img1} />
-            </div>
-            <div>
-                <img src={img2} />
-            </div>
-            <div>
-                <img src={img3} />
-            </div>
-            <div>
-                <img src={img4} />
-            </div>
-            <div>
-                <img src={img5} />
-            </div>
-            <div>
-                <img src={img6} />
-            </div>
-        </Carousel>
+      <Carousel showArrows={false} infiniteLoop autoPlay>
+        <div>
+          <img src={img1} />
+        </div>
+        <div>
+          <img src={img2} />
+        </div>
+        <div>
+          <img src={img3} />
+        </div>
+        <div>
+          <img src={img4} />
+        </div>
+        <div>
+          <img src={img5} />
+        </div>
+        <div>
+          <img src={img6} />
+        </div>
+      </Carousel>
     );
 };
 
