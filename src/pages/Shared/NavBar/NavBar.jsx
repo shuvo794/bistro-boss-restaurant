22908fc6a1b9d3d33f058c775d4f62da4bea3ca3
shuvo794 <shuvohossain794@gmail.com>
2024-01-13 08:18:46 +0600

    edit desgin

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -70,7 +70,7 @@ const NavBar = () => {
               <br />
               Resturent
             </p>
-            {/* <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a> */}
+            
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">{navOptions}</ul>
