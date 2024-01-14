3fd70c6f06c9f3a594db5750989267bf7f7f3ad4
shuvo794 <shuvohossain794@gmail.com>
2024-01-14 10:03:54 +0600

    send

--- src/pages/AddItems/AddItems.jsx
+++ src/pages/AddItems/AddItems.jsx
@@ -1,16 +1,14 @@
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 
-
-
 const AddItems = () => {
-    return (
-      <div>
-        <SectionTitle
-          subHeading={"What's new?"}
-          heading={"ADD AN ITEM"}
-        ></SectionTitle>
-      </div>
-    );
-}
+  return (
+    <div>
+      <SectionTitle
+        subHeading={"What's new?"}
+        heading={"ADD AN ITEM"}
+      ></SectionTitle>
+    </div>
+  );
+};
 
 export default AddItems;
