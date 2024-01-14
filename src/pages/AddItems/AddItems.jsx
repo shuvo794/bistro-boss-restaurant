b136c085a05aed960971c3064e9265740c77688f
shuvo794 <shuvohossain794@gmail.com>
2024-01-14 10:13:50 +0600

    send

--- src/pages/AddItems/AddItems.jsx
+++ src/pages/AddItems/AddItems.jsx
@@ -18,6 +18,21 @@ const AddItems = () => {
             className="input input-bordered w-full max-w-xs"
           />
         </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Category*</span>
+          </div>
+          <select className="select select-bordered">
+            <option disabled selected>
+              Category
+            </option>
+            <option>Star Wars</option>
+            <option>Harry Potter</option>
+            <option>Lord of the Rings</option>
+            <option>Planet of the Apes</option>
+            <option>Star Trek</option>
+          </select>
+        </label>
       </form>
     </div>
   );
