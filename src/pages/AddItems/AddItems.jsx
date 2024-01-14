c94812cba8a823068c634b5b28b75a6222328247
shuvo794 <shuvohossain794@gmail.com>
2024-01-14 10:08:23 +0600

    send

--- src/pages/AddItems/AddItems.jsx
+++ src/pages/AddItems/AddItems.jsx
@@ -7,6 +7,18 @@ const AddItems = () => {
         subHeading={"What's new?"}
         heading={"ADD AN ITEM"}
       ></SectionTitle>
+      <form>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Recipe name*</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+      </form>
     </div>
   );
 };
