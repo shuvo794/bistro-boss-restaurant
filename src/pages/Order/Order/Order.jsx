5b55a3def2daa1dffc76e3afe11932bbde425af0
shuvo794 <shuvohossain794@gmail.com>
2024-01-15 11:41:43 +0600

    send

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -9,7 +9,7 @@ import { useParams } from 'react-router';
 import { Helmet } from 'react-helmet-async';
 
 const Order = () => {
-    const categories = ['salad', 'pizza', 'soup', 'dessert', 'drinks'];
+    const categories = ["salad", "pizza", "soup", "dessert", "drinks", "Desi"];
     const { category } = useParams();
     const initialIndex = categories.indexOf(category);
     const [tabIndex, setTabIndex] = useState(initialIndex);
@@ -19,39 +19,45 @@ const Order = () => {
     const soup = menu.filter(item => item.category === 'soup');
     const salad = menu.filter(item => item.category === 'salad');
     const pizza = menu.filter(item => item.category === 'pizza');
+    const desi = menu.filter((item) => item.category === "Desi");
     const drinks = menu.filter(item => item.category === 'drinks');
+   
 
     return (
-        <div>
-            <Helmet>
-                <title>Bistro Boss | Order Food</title>
-            </Helmet>
-            <Cover img={orderCoverImg} title="Order Food"></Cover>
-            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-                <TabList>
-                    <Tab>Salad</Tab>
-                    <Tab>Pizza</Tab>
-                    <Tab>Soup</Tab>
-                    <Tab>Dessert</Tab>
-                    <Tab>Drinks</Tab>
-                </TabList>
-                <TabPanel>
-                    <OrderTab items={salad}></OrderTab>
-                </TabPanel>
-                <TabPanel>
-                    <OrderTab items={pizza}></OrderTab>
-                </TabPanel>
-                <TabPanel>
-                    <OrderTab items={soup}></OrderTab>
-                </TabPanel>
-                <TabPanel>
-                    <OrderTab items={desserts}></OrderTab>
-                </TabPanel>
-                <TabPanel>
-                    <OrderTab items={drinks}></OrderTab>
-                </TabPanel>
-            </Tabs>
-        </div>
+      <div>
+        <Helmet>
+          <title>Bistro Boss | Order Food</title>
+        </Helmet>
+        <Cover img={orderCoverImg} title="Order Food"></Cover>
+        <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+          <TabList>
+            <Tab>Salad</Tab>
+            <Tab>Pizza</Tab>
+            <Tab>Soup</Tab>
+            <Tab>Dessert</Tab>
+            <Tab>desi</Tab>
+            <Tab>Drinks</Tab>
+          </TabList>
+          <TabPanel>
+            <OrderTab items={salad}></OrderTab>
+          </TabPanel>
+          <TabPanel>
+            <OrderTab items={pizza}></OrderTab>
+          </TabPanel>
+          <TabPanel>
+            <OrderTab items={soup}></OrderTab>
+          </TabPanel>
+          <TabPanel>
+            <OrderTab items={desserts}></OrderTab>
+          </TabPanel>
+          <TabPanel>
+            <OrderTab items={desi}></OrderTab>
+          </TabPanel>
+          <TabPanel>
+            <OrderTab items={drinks}></OrderTab>
+          </TabPanel>
+        </Tabs>
+      </div>
     );
 };
 
