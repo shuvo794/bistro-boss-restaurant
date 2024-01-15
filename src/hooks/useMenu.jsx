e6fa1f5add4f77360f787b5d6cc6c52e81dc0dba
shuvo794 <shuvohossain794@gmail.com>
2024-01-15 12:22:08 +0600

    useMenu Refactor

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,25 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 
-
 const useMenu = () => {
-    // const [menu, setMenu] = useState([]);
-    // const [loading, setLoading] = useState(true);
-    // useEffect(() => {
-    //     fetch('http://localhost:5000/menu')
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             setMenu(data);
-    //             setLoading(false);
-    //         });
-    // }, [])
-    const { data: menu = [], isLoading : loading } = useQuery({
-      queryKey: ["menu"],
-      queryFn: async () => {
-        const res = await fetch("http://localhost:5000/menu");
-        return res.json();
-      },
-    });
-    return [menu, loading]
-}
+  const { data: menu = [], isLoading: loading } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/menu");
+      return res.json();
+    },
+  });
+  return [menu, loading];
+};
 
 export default useMenu;
