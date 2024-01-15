d27856e081854eadcd6d7f3546809d3e8d24cae1
shuvo794 <shuvohossain794@gmail.com>
2024-01-15 11:54:01 +0600

    send

--- src/pages/Dashboard/ManegItem/ManegItem.jsx
+++ src/pages/Dashboard/ManegItem/ManegItem.jsx
@@ -1,6 +1,10 @@
+import SectionTittle from "../../../components/SectionTitle/SectionTitle";
 
 const ManegItem = () => {
-    
-}
+  <SectionTittle
+    subHeading={"Should Try"}
+    heading={"RECOMMENDS"}
+  ></SectionTittle>;
+};
 
 export default ManegItem;
