241ee72e83bb1c4669b1f1cc215e4e160836899f
shuvo794 <shuvohossain794@gmail.com>
2024-01-16 13:21:13 +0600

    send a error code

--- src/pages/Dashboard/Payment/CheckoutForm/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm/CheckoutForm.jsx
@@ -1,22 +1,37 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
-    const stripe = useStripe();
-     const elements = useElements();
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-          if (!stripe || !elements) {
-            return;
-        }
-        
-        const card = elements.getElement(CardElement);
-        
-        if (card === null) {
-            return;
-        }
-        
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState("");
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+      return;
     }
-    return (
+
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("error", error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+      console.log("PaymentMethod", paymentMethod);
+    }
+  };
+  return (
+    <>
       <form className="w-2/3 m-8" onSubmit={handleSubmit}>
         <CardElement
           options={{
@@ -34,11 +49,17 @@ const CheckoutForm = () => {
             },
           }}
         />
-        <button type="submit" className="btn btn-primary btn-sm mt-4" disabled={!stripe}>
+        <button
+          type="submit"
+          className="btn btn-primary btn-sm mt-4"
+          disabled={!stripe}
+        >
           Pay
         </button>
       </form>
-    );
-}
+      {cardError && <p className="text-red-600">{cardError}</p>}
+    </>
+  );
+};
 
 export default CheckoutForm;
