cc07311e0db37678cda9761161878cc4c79c070a
shuvo794 <shuvohossain794@gmail.com>
2024-01-16 11:04:56 +0600

    stipe set up

--- src/pages/Dashboard/Payment/CheckoutForm/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm/CheckoutForm.jsx
@@ -1,5 +1,21 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 const CheckoutForm = () => {
+    const stripe = useStripe();
+     const elements = useElements();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+          if (!stripe || !elements) {
+            return;
+        }
+        
+        const card = elements.getElement(CardElement);
+        
+        if (card === null) {
+            return;
+        }
+        
+    }
     return (
       <form onSubmit={handleSubmit}>
         <CardElement
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,7 +1,17 @@
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm/CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+
+// Todo Publication key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_Pk);
+
 const Payment = () => {
   return (
     <div>
       <h1 className="text-center font-bold text-3xl">PAYMENT</h1>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm></CheckoutForm>
+      </Elements>
     </div>
   );
 };
