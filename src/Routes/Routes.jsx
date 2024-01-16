6594a7e940976e1334fcc6e9af33e9b5e51718fa
shuvo794 <shuvohossain794@gmail.com>
2024-01-16 09:58:35 +0600

    work payment method

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -14,6 +14,7 @@ import AddItems from "../pages/AddItems/AddItems";
 import AdminRoute from "./AdminRoute";
 import ManegItem from "../pages/Dashboard/ManegItem/ManegItem";
 import UpdateValue from "../pages/Dashboard/ManegItem/UpdateValue";
+import Payment from "../pages/Dashboard/Payment/Payment";
 
 export const router = createBrowserRouter([
   {
@@ -61,6 +62,10 @@ export const router = createBrowserRouter([
       {
         path: "cart",
         element: <Cart></Cart>,
+      },
+      {
+        path: "payment",
+        element: <Payment></Payment>,
       },
 
       // admin routes
