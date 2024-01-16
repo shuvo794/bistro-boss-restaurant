a11aaf8a1dfec584cf680c8fc087906bc75cf7d1
shuvo794 <shuvohossain794@gmail.com>
2024-01-16 09:58:14 +0600

    work payment method

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,5 +1,11 @@
 
 
 const Payment = () => {
-    
+    return (
+      <div>
+        <h1 className="text-center font-bold text-3xl">PAYMENT</h1>
+      </div>
+    );
 }
+
+export default Payment;
