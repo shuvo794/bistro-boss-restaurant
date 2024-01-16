60ee11b12677e7b4b48eecb4fa1ef354390fb53c
shuvo794 <shuvohossain794@gmail.com>
2024-01-16 07:50:44 +0600

    finsh a manage section admin

--- src/pages/Dashboard/ManegItem/ManegItem.jsx
+++ src/pages/Dashboard/ManegItem/ManegItem.jsx
@@ -1,15 +1,96 @@
+import { FaTrashAlt } from "react-icons/fa";
 import SectionTittle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManegItem = () => {
-    const [menu] = useMenu();
+  const [menu, , refetch] = useMenu();
+  const axiosSecure = useAxiosSecure();
+  const handleDeleteItem = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.delete(`/menu/${item._id}`);
+        // console.log(res.data);
+        if (res.data.deletedCount > 0) {
+          // refetch to update the ui
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${item.name} has been deleted`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      }
+    });
+  };
   return (
     <div>
       <SectionTittle
         subHeading={"Hurry Up"}
         heading={"MANAGE ALL ITEMS"}
       ></SectionTittle>
-      ;
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>ITEM IMAGE</th>
+              <th>ITEM NAME</th>
+              <th>PRICE</th>
+              <th>ACTION</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {menu.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{item.name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{item.category}</td>
+                <td className="text-right">${item.price}</td>
+                <td>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteItem(item)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
