8c5e789821e003028140e8dc8473e82739ad54d2
shuvo794 <shuvohossain794@gmail.com>
2024-01-16 10:27:15 +0600

    create CheckoutForm

--- src/pages/Dashboard/Payment/CheckoutForm/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm/CheckoutForm.jsx
@@ -1,10 +1,28 @@
 
 const CheckoutForm = () => {
     return (
-        <div>
-
-        </div>
-    )
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button type="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
+    );
 }
 
 export default CheckoutForm;
