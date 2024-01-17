d2cbbefbedb0cbc3b5307d382fe81fa7f49f9d75
shuvo794 <shuvohossain794@gmail.com>
2024-01-17 08:29:32 +0600

    send

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,7 +2,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
 import { loadStripe } from "@stripe/stripe-js";
 
-// Todo Publication key
+
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_Pk);
 
 const Payment = () => {
