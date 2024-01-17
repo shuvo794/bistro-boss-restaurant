a07d89bfc6f5fb953826e493dd2ffabf43aaa9f7
shuvo794 <shuvohossain794@gmail.com>
2024-01-17 09:16:43 +0600

    send

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,11 +1,15 @@
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
 import { loadStripe } from "@stripe/stripe-js";
+import useCart from "../../../hooks/useCart";
 
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_Pk);
 
 const Payment = () => {
+  const [cart] = useCart();
+  const total = cart.reduce((sum, item) => sum + item.price, 0)
+  
   return (
     <div>
       <h1 className="text-center font-bold text-3xl">PAYMENT</h1>
