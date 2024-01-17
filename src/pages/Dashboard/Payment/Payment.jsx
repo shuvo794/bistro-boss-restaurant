7869091eafdae34d0ff7a06f6fc03110d796d924
shuvo794 <shuvohossain794@gmail.com>
2024-01-17 09:25:04 +0600

    send

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -9,12 +9,13 @@ const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_Pk);
 const Payment = () => {
   const [cart] = useCart();
   const total = cart.reduce((sum, item) => sum + item.price, 0)
+  const price = parseFloat(total.toFixed(2));
   
   return (
     <div>
       <h1 className="text-center font-bold text-3xl">PAYMENT</h1>
       <Elements stripe={stripePromise}>
-        <CheckoutForm></CheckoutForm>
+        <CheckoutForm price={price}></CheckoutForm>
       </Elements>
     </div>
   );
