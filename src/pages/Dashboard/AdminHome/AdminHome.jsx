18e909004d2abcd591333f5f0256fb1897ec1354
shuvo794 <shuvohossain794@gmail.com>
2024-01-20 11:02:34 +0600

    send

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -25,9 +25,9 @@ const AdminHome = () => {
     <div className="stat-figure text-secondary">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
     </div>
-    <div className="stat-title">Downloads</div>
-    <div className="stat-value">31K</div>
-    <div className="stat-desc">Jan 1st - Feb 1st</div>
+    <div className="stat-title">Revenue</div>
+                    <div className="stat-value">{ stats.revinew}</div>
+    {/* <div className="stat-desc">Jan 1st - Feb 1st</div> */}
   </div>
   
   <div className="stat">
