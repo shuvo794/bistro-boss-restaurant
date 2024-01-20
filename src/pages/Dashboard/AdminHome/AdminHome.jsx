38db5b9c814351b17777fc1c2b039c887642c388
shuvo794 <shuvohossain794@gmail.com>
2024-01-20 10:41:17 +0600

    send

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,9 +1,11 @@
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 
 const AdminHome = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
     return (
         <div>
             <h1 className="text-3xl font-bold">Welcome Back {user?.displayName}</h1>
