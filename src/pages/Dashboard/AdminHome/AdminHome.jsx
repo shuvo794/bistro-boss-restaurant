76b05009fdb0d5ee9ad52bfa1ef0cb23e10e620e
shuvo794 <shuvohossain794@gmail.com>
2024-01-20 11:24:40 +0600

    send

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,6 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import {  useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaUsers, FaWallet } from "react-icons/fa";
+import { LiaTruckPickupSolid } from "react-icons/lia";
 
 
 
@@ -23,7 +25,8 @@ const AdminHome = () => {
   
   <div className="stat">
     <div className="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+      <FaWallet className="text-lg	" />
+
     </div>
     <div className="stat-title">Revenue</div>
                     <div className="stat-value">{ stats.revinew}</div>
@@ -32,20 +35,30 @@ const AdminHome = () => {
   
   <div className="stat">
     <div className="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+      <FaUsers />
+
     </div>
-    <div className="stat-title">New Users</div>
-    <div className="stat-value">4,200</div>
-    <div className="stat-desc">↗︎ 400 (22%)</div>
+    <div className="stat-title">Customers</div>
+                    <div className="stat-value">{ stats.users}</div>
+   
   </div>
   
   <div className="stat">
     <div className="stat-figure text-secondary">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
     </div>
-    <div className="stat-title">New Registers</div>
-    <div className="stat-value">1,200</div>
-    <div className="stat-desc">↘︎ 90 (14%)</div>
+    <div className="stat-title">Products</div>
+                    <div className="stat-value">{ stats.products}</div>
+  
+                </div>
+                <div className="stat">
+    <div className="stat-figure text-secondary">
+      <LiaTruckPickupSolid />
+
+    </div>
+    <div className="stat-title">Orders</div>
+                    <div className="stat-value">{stats.orders}</div>
+   
   </div>
   
 </div>
