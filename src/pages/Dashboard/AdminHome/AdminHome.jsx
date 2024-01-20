04cc31b3e5208b78cd930486699b7297718c10be
shuvo794 <shuvohossain794@gmail.com>
2024-01-20 10:44:49 +0600

    send

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
@@ -6,6 +7,12 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 const AdminHome = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
+    const { } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure('/admin-stats')
+        }
+    })
     return (
         <div>
             <h1 className="text-3xl font-bold">Welcome Back {user?.displayName}</h1>
