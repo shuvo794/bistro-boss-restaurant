fb9eab39b60f6042878a298115f6055535c802e3
shuvo794 <shuvohossain794@gmail.com>
2024-01-21 22:28:44 +0600

    send a solve code

--- src/pages/Dashboard/UserHome/UserHome.jsx
+++ src/pages/Dashboard/UserHome/UserHome.jsx
@@ -1,11 +1,13 @@
 import useAuth from "../../../hooks/useAuth";
 
 
+
 const UserHome = () => {
     const { user } = useAuth();
+    console.log(user,"test")
     return (
         <div>
-            <h1>Welcome back User  {user.displayName}</h1>
+           <h1 className="text-3xl font-bold">Welcome Back {user?.displayName}</h1>
         </div>
     )
 }
