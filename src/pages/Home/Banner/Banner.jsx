344c0471357bbe6251df4be2e6d9b73ebb45ac76
shuvo794 <shuvohossain794@gmail.com>
2024-01-29 13:40:55 +0600

    send

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,23 +2,34 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import img1 from '../../../assets/home/01.jpg';
 import img2 from '../../../assets/home/02.jpg';
-import img3 from '../../../assets/home/03.png';
+import img3 from '../../../assets/home/banner.jpg';
 import img4 from '../../../assets/home/04.jpg';
 import img5 from '../../../assets/home/05.png';
 import img6 from '../../../assets/home/06.png';
 
 const Banner = () => {
     return (
-      <Carousel showArrows={false} infiniteLoop autoPlay>
+      <Carousel showThumbs={false}	 showArrows={false} infiniteLoop autoPlay>
+        
+        <div>
+          <img src={img2} />
+
+        </div>
         <div>
           <img src={img1} />
         </div>
+       
         <div>
-          <img src={img2} />
+          <img src={img4} />
         </div>
-        {/* <div>
-          <img src={img3} />
-        </div> */}
+         <div>
+          <img src={img2} />
+
+        </div>
+        <div>
+          <img src={img1} />
+        </div>
+       
         <div>
           <img src={img4} />
         </div>
