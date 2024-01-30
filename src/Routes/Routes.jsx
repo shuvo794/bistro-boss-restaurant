b17908229d6d425be7ab2484e91b23bcf8b19d92
shuvo794 <shuvohossain794@gmail.com>
2024-01-30 22:00:27 +0600

    depoly server site run

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -114,7 +114,7 @@ export const router = createBrowserRouter([
       {
           path: 'updateItem/:id',
           element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
-          loader: ({params}) => fetch(`http://localhost:5000/menu/${params.id}`)
+          loader: ({params}) => fetch(`https://bistro-boss-server-tawny-xi.vercel.app/menu/${params.id}`)
         },
       
     ],
