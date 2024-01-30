15044752b5ef0a01c382b79256bd363483e8d8b9
shuvo794 <shuvohossain794@gmail.com>
2024-01-30 09:50:34 +0600

    change image

--- src/pages/Home/Chef_Say/Chef_Say.jsx
+++ src/pages/Home/Chef_Say/Chef_Say.jsx
@@ -11,8 +11,8 @@ const Chef_Say = () => {
         heading={"RECOMMENDS"}
       ></SectionTittle>
 
-      <div className="grid md:grid-cols-3 gap-5 ">
-        <div className="card w-96 glass">
+      <div className="grid md:grid-cols-3 gap-5 p-2 ">
+        <div className="card w-86 glass">
           <img
             style={{ width: "424px", height: "300px" }}
             src={slide2}
@@ -26,10 +26,10 @@ const Chef_Say = () => {
             </div>
           </div>
         </div>
-        <div className="card w-96 glass">
+        <div className="card w-86 glass">
           <img
             style={{ width: "424px", height: "300px" }}
-            src={slide1}
+            src={slide3}
             alt="car!"
           />
           <div className="card-body ">
@@ -40,10 +40,10 @@ const Chef_Say = () => {
             </div>
           </div>
         </div>
-        <div className="card w-96 glass">
+        <div className="card w-86 glass">
           <img
             style={{ width: "424px", height: "300px" }}
-            src={slide3}
+            src={slide1}
             alt="car!"
           />
           <div className="card-body ">
