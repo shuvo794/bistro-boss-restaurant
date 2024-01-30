c571aab58953bd6b8cd844c33523aaf802c20fde
shuvo794 <shuvohossain794@gmail.com>
2024-01-30 09:10:41 +0600

    change image

--- src/pages/Home/Chef_Say/Chef_Say.jsx
+++ src/pages/Home/Chef_Say/Chef_Say.jsx
@@ -1,5 +1,7 @@
 import SectionTittle from "../../../components/SectionTitle/SectionTitle";
 import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
 
 const Chef_Say = () => {
   return (
@@ -41,7 +43,7 @@ const Chef_Say = () => {
         <div className="card w-96 glass">
           <img
             style={{ width: "424px", height: "300px" }}
-            src={slide1}
+            src={slide3}
             alt="car!"
           />
           <div className="card-body ">
