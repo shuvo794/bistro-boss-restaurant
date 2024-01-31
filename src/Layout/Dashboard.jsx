2cc433975d49baf2762b45f5d70e551a9d9fe537
shuvo794 <shuvohossain794@gmail.com>
2024-01-31 12:23:07 +0600

    change a route

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -46,7 +46,7 @@ const Dashboard = () => {
                 </NavLink>
               </li>
               <li>
-                <NavLink to="/dashboard/bookings">
+                <NavLink to="/">
                   <FaBook></FaBook>
                   Manage Bookings
                 </NavLink>
