76c1224c0fb064ee948c93f3f27ae2d29a82d813
shuvo794 <shuvohossain794@gmail.com>
2024-02-01 12:47:44 +0600

    edit desgin

--- src/pages/Dashboard/UserHome/UserHome.jsx
+++ src/pages/Dashboard/UserHome/UserHome.jsx
@@ -1,12 +1,13 @@
 import useAuth from "../../../hooks/useAuth";
 import { FaUserCircle } from "react-icons/fa";
+import useCart from "../../../hooks/useCart";
 
 
 
 
 const UserHome = () => {
     const { user } = useAuth();
-  
+   const [cart] = useCart();
    
     return (
         <div>
@@ -25,7 +26,7 @@ const UserHome = () => {
                         <h2 className="uppercase text-lg bg-[#c23616] text-white text-center rounded-lg p-1" >Name : {user?.displayName}</h2>
                         <p className="uppercase text-sm bg-[#c23616] text-white text-center rounded-lg mt-5 p-1">Email : {user?.email}</p>
                     <div className="card-actions justify-end">
-                            <button className="btn btn-primary mt-5">Order </button>
+                            <button className="btn btn-primary mt-5">Order - {cart.length} </button>
     </div>
                     </div>
    
