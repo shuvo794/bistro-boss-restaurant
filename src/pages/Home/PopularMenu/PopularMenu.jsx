207cc1c871e56427a4327445f8b4063fe19425af
shuvo794 <shuvohossain794@gmail.com>
2024-02-01 13:06:25 +0600

    edit desgin

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -9,14 +9,15 @@ const PopularMenu = () => {
     const popular = menu.filter(item => item.category === 'popular');
     
     return (
-        <section className="mb-12">
+        <section className="mb-12 ">
             <SectionTitle
                 heading="From Our Menu"
                 subHeading="Popular Items"
             ></SectionTitle>
-            <div className="grid md:grid-cols-2 gap-10">
+            <div className="grid md:grid-cols-2 gap-10 ">
                 {
                     popular.map(item => <MenuItem
+                      
                         key={item._id}
                         item={item}
                     ></MenuItem>)
