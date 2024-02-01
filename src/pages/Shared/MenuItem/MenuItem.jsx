7f00aa4cc415ec884a47a1076c991aa803ac2b50
shuvo794 <shuvohossain794@gmail.com>
2024-02-01 13:07:26 +0600

    edit desgin

--- src/pages/Shared/MenuItem/MenuItem.jsx
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -3,7 +3,7 @@
 const MenuItem = ({item}) => {
     const {name, image, price, recipe} = item;
     return (
-        <div className="flex space-x-2">
+        <div className="flex space-x-2   m-5">
             <img style={{borderRadius: '0 200px 200px 200px'}} className="w-[100px]" src={image} alt="" />
             <div>
                 <h3 className="uppercase">{name}----------</h3>
