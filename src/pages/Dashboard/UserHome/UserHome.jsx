71d174b1f25a9c472e65d6fdc76ab40178cd7dc4
shuvo794 <shuvohossain794@gmail.com>
2024-02-03 22:10:58 +0600

    edit contend

--- src/pages/Dashboard/UserHome/UserHome.jsx
+++ src/pages/Dashboard/UserHome/UserHome.jsx
@@ -14,17 +14,13 @@ const UserHome = () => {
     return (
         <div>
             <h1 className="text-3xl font-bold bg-[#c23616] text-white p-2 text-center">Welcome Back User Home page</h1>
-            
-            {/* User Profile  */}
-
-            <div className="card card-side bg-base-100 shadow-xl mt-16">
+             <div className="card card-side bg-base-100 shadow-xl mt-16">
                 <figure className="w-1/3">
                     <FaUserCircle className="text-[#c23616] 	" />
 
                 </figure>
-  <div className="card-body w-1/2">
-    
-                    <div className="mt-16">
+                   <div className="card-body w-1/2">
+                     <div className="mt-16">
                         <h2 className="uppercase text-lg bg-[#c23616] text-white text-center rounded-lg p-1" >Name : {user?.displayName}</h2>
                         <p className="uppercase text-sm bg-[#c23616] text-white text-center rounded-lg mt-5 p-1">Email : {user?.email}</p>
                     <div className="card-actions justify-end">
@@ -34,12 +30,11 @@ const UserHome = () => {
                     <div className="badge bg-primary">+{cart.length}</div>
                 </button>
             </Link>
-    </div>
-                    </div>
-   
-  </div>
+            </div>
+         </div>
+   </div>
 </div>
-        </div>
+</div>
     )
 }
 
