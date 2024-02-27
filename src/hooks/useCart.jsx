12e3ef1115cbe1316bd944db184f4e92eb7812d3
shuvo794 <shuvohossain794@gmail.com>
2024-02-27 11:41:33 +0600

    code formatting

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -3,17 +3,17 @@ import useAxiosSecure from "./useAxiosSecure";
 import useAuth from "./useAuth";
 
 const useCart = () => {
-    const axiosSecure = useAxiosSecure();
-    const { user} = useAuth();
-    const { refetch, data: cart = [] } = useQuery({
-        queryKey: ['cart', user?.email],
-        queryFn: async() => {
-            const res = await axiosSecure.get(`/carts?email=${user?.email}`);
-            return res.data;
-        }
-    })
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  const { refetch, data: cart = [] } = useQuery({
+    queryKey: ["cart", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/carts?email=${user?.email}`);
+      return res.data;
+    },
+  });
 
-    return [cart, refetch]
+  return [cart, refetch];
 };
 
 export default useCart;
--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,14 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 
 const useMenu = () => {
-  const { data: menu = [], isLoading: loading,refetch } = useQuery({
+  const {
+    data: menu = [],
+    isLoading: loading,
+    refetch,
+  } = useQuery({
     queryKey: ["menu"],
     queryFn: async () => {
       const res = await fetch("http://localhost:5000/menu");
       return res.json();
     },
   });
-  return [menu, loading,refetch];
+  return [menu, loading, refetch];
 };
 
 export default useMenu;
