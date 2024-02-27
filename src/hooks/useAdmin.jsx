f05f8963c499f6fdfee1c0b3bd740f7ebf50f21d
shuvo794 <shuvohossain794@gmail.com>
2024-02-27 11:40:34 +0600

    code formatting

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -3,14 +3,14 @@ import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin = () => {
-  const { user,loading } = useAuth();
+  const { user, loading } = useAuth();
   const axiosSecure = useAxiosSecure();
   const { data: isAdmin, isPending: isAdminLoading } = useQuery({
     queryKey: [user?.email, "isAdmin"],
-    enabled:!loading,
+    enabled: !loading,
     queryFn: async () => {
-       const res = await axiosSecure.get(`/users/admin/${user?.email}`);
-       console.log(res.data);
+      const res = await axiosSecure.get(`/users/admin/${user?.email}`);
+      console.log(res.data);
       return res.data?.admin;
     },
   });
