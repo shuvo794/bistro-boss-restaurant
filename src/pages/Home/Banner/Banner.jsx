70c625275076a2c436f9deda57e682c2ecdbb3da
shuvo794 <shuvohossain794@gmail.com>
2024-02-27 11:40:54 +0600

    code formatting

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,38 +1,42 @@
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import img1 from '../../../assets/home/01.jpg';
-import img2 from '../../../assets/home/02.jpg';
-import img4 from '../../../assets/home/04.jpg';
+import img1 from "../../../assets/home/01.jpg";
+import img2 from "../../../assets/home/02.jpg";
+import img4 from "../../../assets/home/04.jpg";
 
 const Banner = () => {
-    return (
-      <Carousel showThumbs={true}  dynamicHeight thumbWidth={200} thumbHeight={50}	 showArrows={false} infiniteLoop autoPlay>
-        
-        <div>
-          <img src={img2} />
+  return (
+    <Carousel
+      showThumbs={true}
+      dynamicHeight
+      thumbWidth={200}
+      thumbHeight={50}
+      showArrows={false}
+      infiniteLoop
+      autoPlay
+    >
+      <div>
+        <img src={img2} />
+      </div>
+      <div>
+        <img src={img1} />
+      </div>
 
-        </div>
-        <div>
-          <img src={img1} />
-        </div>
-       
-        <div>
-          <img src={img4} />
-        </div>
-         <div>
-          <img src={img2} />
+      <div>
+        <img src={img4} />
+      </div>
+      <div>
+        <img src={img2} />
+      </div>
+      <div>
+        <img src={img1} />
+      </div>
 
-        </div>
-        <div>
-          <img src={img1} />
-        </div>
-       
-        <div>
-          <img src={img4} />
-        </div>
-       
-      </Carousel>
-    );
+      <div>
+        <img src={img4} />
+      </div>
+    </Carousel>
+  );
 };
 
 export default Banner;
