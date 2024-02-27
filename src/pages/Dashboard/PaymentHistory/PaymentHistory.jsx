40fdb01594a34eca94ccfeedc5fb93f08db6918a
shuvo794 <shuvohossain794@gmail.com>
2024-02-27 11:46:12 +0600

    code formatting

--- src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,47 +1,49 @@
 import { useQuery } from "@tanstack/react-query";
 
-
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentHistory = () => {
-    const { user } = useAuth();
-    const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
 
-    const { data: payments = [] } = useQuery({
-        queryKey: ['payments', user?.email],
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/payments/${user?.email}`)
-            return res.data;
-        }
-    })
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments/${user?.email}`);
+      return res.data;
+    },
+  });
 
-    return (
-        <div >
-            <h2 className="text-3xl mb-8 font-bold">Total Payments: {payments.length}</h2>
-            <div className="overflow-x-auto ">
-                <table className="table table-zebra  bg-[#c23616] w-full">
-                    <thead className="bg-[#c23616]">
-                        <tr >
-                            <th className=" bg-[#c23616] text-white">#</th>
-                            <th className=" bg-[#c23616] text-white">price</th>
-                            <th className=" bg-[#c23616] text-white">Transaction Id</th>
-                            <th className=" bg-[#c23616] text-white">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {payments.map((payment, index) => <tr key={payment._id}>
-                            <th>{index + 1}</th>
-                            <td>${payment.price}</td>
-                            <td>{payment.tranjuctionId}</td>
-                            <td>{payment.status}</td>
-                        </tr>)}
-                        
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-3xl mb-8 font-bold">
+        Total Payments: {payments.length}
+      </h2>
+      <div className="overflow-x-auto ">
+        <table className="table table-zebra  bg-[#c23616] w-full">
+          <thead className="bg-[#c23616]">
+            <tr>
+              <th className=" bg-[#c23616] text-white">#</th>
+              <th className=" bg-[#c23616] text-white">price</th>
+              <th className=" bg-[#c23616] text-white">Transaction Id</th>
+              <th className=" bg-[#c23616] text-white">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {payments.map((payment, index) => (
+              <tr key={payment._id}>
+                <th>{index + 1}</th>
+                <td>${payment.price}</td>
+                <td>{payment.tranjuctionId}</td>
+                <td>{payment.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentHistory;
--- src/pages/Home/Call_Us/Call_Us.jsx
+++ src/pages/Home/Call_Us/Call_Us.jsx
@@ -1,15 +1,13 @@
-
-
 const Call_Us = () => {
-    return (
-      <div className="hero min-h-[250px] bg-black text-white">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-3xl font-bold">Call Us: 01876864289</h1>
-          </div>
+  return (
+    <div className="hero min-h-[250px] bg-black text-white">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-3xl font-bold">Call Us: 01876864289</h1>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Call_Us
+export default Call_Us;
